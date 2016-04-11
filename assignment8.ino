19051c1b63eeb56127e4c69ae4886ca4dd01c7d9
evid27 <evi270103@yahoo.gr>
2016-04-11 20:32:22 +0300

    Update assignment8.ino

--- assignment8.ino
+++ assignment8.ino
@@ -24,6 +24,12 @@ void setup() {
 void loop() {
   folkracer.updateMotors(); //διατήρησε την ταχύτητα του αυτοκινήτου σταθερή
   int distance = front.getDistance(); //μέτρησε την απόσταση και αποθήκευσέ την στη μεταβλητή distance
-
+  
   //TO-DO: Κάντε το αυτοκινητάκι να στρίβει 90 μοίρες προς τα αριστερά όταν βρίσκει ένα κοντινό εμπόδιο και να συνεχίζει την πορεία του
+  if (distance < 10 && distance > 0) {
+    folkracer.rotate(-90);
+    folkracer.setSpeed(0.5);
+  }  
+  }
+  
 }
